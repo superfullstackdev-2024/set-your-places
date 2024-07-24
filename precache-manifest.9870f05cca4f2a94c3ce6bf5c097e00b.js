be0208f3c12795437057de0041c1742eb7cbaf3d
superfullstackdev-2024 <superfullstackdev@gmail.com>
2024-07-24 12:24:37 +0300

    Updates

---
+++ precache-manifest.9870f05cca4f2a94c3ce6bf5c097e00b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac76a8736ffb2b469d1908f5f6b24a4",
+    "url": "/set-your-places/index.html"
+  },
+  {
+    "revision": "6c12e55dad518d17c807",
+    "url": "/set-your-places/static/css/main.ac4974b9.chunk.css"
+  },
+  {
+    "revision": "9f47e448d7746430ee7d",
+    "url": "/set-your-places/static/js/2.2d5ba2d5.chunk.js"
+  },
+  {
+    "revision": "6c12e55dad518d17c807",
+    "url": "/set-your-places/static/js/main.71b08ec5.chunk.js"
+  },
+  {
+    "revision": "96e0a70673e486c2e3eb",
+    "url": "/set-your-places/static/js/runtime-main.ab5f01ea.js"
+  }
+]);
